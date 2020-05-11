17435e8e2f14aa54ce183f689a8e133e80dc0f3e
JIN <a1824jj@aiit.ac.jp>
2020-05-11 07:28:18 +0000

    using mutation & change value

--- hello-nuxtjs/nuxt-vuex/pages/index.vue
+++ hello-nuxtjs/nuxt-vuex/pages/index.vue
@@ -2,6 +2,7 @@
   <div class="container">
     <div>
       <p>{{ $store.state.message }}</p>
+      <button v-on:click="$store.commit('updateMessage')">Update</button>
     </div>
   </div>
 </template>
