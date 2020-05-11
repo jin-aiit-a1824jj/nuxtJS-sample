3d4f0918a3b9bde5e9d66c7095fd8d3ba2c55f62
JIN <a1824jj@aiit.ac.jp>
2020-05-11 03:47:42 +0000

    using Mustache

--- hello-nuxtjs/hello-nuxt/pages/index.vue
+++ hello-nuxtjs/hello-nuxt/pages/index.vue
@@ -1,13 +1,18 @@
 <template>
   <section class="container">
-    <p>Hello World!</p>
+    <p>Hello World! = hot reloading!</p>
+    <p>{{ message }}</p>
   </section>
 </template>
 
 <script>
 
 export default {
-
+  data: function(){
+    return {
+      message: 'マスタッシュ構文を使う!'
+    }
+  }
 }
 </script>
 
