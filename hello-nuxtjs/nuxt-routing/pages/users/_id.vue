9f8d5f76c6ee4300383f5cb82109f057d90caa41
JIN <a1824jj@aiit.ac.jp>
2020-05-11 04:27:00 +0000

    dynamic routing

---
+++ hello-nuxtjs/nuxt-routing/pages/users/_id.vue
@@ -0,0 +1,16 @@
+<template>
+  <section>
+    <h1>{{ message }}</h1>
+    <h1>id: {{ this.$route.params.id }}</h1>
+  </section>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      message: '/users/_id.vueを表示中'
+    }
+  }
+}
+</script>
